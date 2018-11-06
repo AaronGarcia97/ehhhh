d7e586d64d641a3d6ab7370334c1814790d4126d
Aaron Garcia <aarongarciag97@gmail.com>
2018-11-05 19:34:37 -0600

    Added mySQL config and example of call

--- server.py
+++ server.py
@@ -1,12 +1,28 @@
 from flask import jsonify
 from flask import Flask
-from flask_cors import CORS
 from flask import request
 
+from flaskext.mysql import MySQL
+from flask_cors import CORS
+
 app = Flask(__name__)
+mysql = MySQL(app)
 CORS(app)
+
+# MySQL config
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'toor'
+app.config['MYSQL_DATABASE_DB'] = 'TaxiUnico'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+mysql.init_app(app)
 
 #Scan a single device, receive ip address
 @app.route("/")
 def hello():
-	return "hello world"
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM Admin WHERE username = \"AaronAdmin\";")
+    data = cursor.fetchall()
+    return str(data)
