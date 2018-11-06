505d6cec26163535bb69af1cf134dc08ffcc69a1
Aaron Garcia <aarongarciag97@gmail.com>
2018-11-06 12:08:56 -0600

    Added working login checker, missing correct response dispatcher

--- server.py
+++ server.py
@@ -23,6 +23,43 @@ def hello():
     conn = mysql.connect()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM Admin WHERE username = \"AaronAdmin\";")
-    data = cursor.fetchall()
-    return str(data)
+    cursor.execute("SELECT * FROM Cliente WHERE username = \"AaronUser\";")
+    data = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return str(data[1])
+
+
+# Validates user exists so it can login
+# If credentials are valid, send home page.
+@app.route("/login", methods = ['POST'])
+def login():
+    # Connect to db and get cursor
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    # Get parameters from post request
+    username = str(request.args.get('username'))
+    password = str(request.args.get('password'))
+
+    # Make query and fetch result
+    cursor.execute("SELECT * FROM Cliente WHERE username = \"" + username + "\";")
+    dataUser = cursor.fetchone()
+
+    # Assign db data variables
+    dbUser = str(dataUser[1])
+    dbPassword = str(dataUser[2])
+
+    # Log data
+    print ("User input\n username: " + username + " \n password: " + password)
+    print ("Db data\n username: " + dbUser + " \n password: " + dbPassword)
+
+    # Close db connection and cursor
+    cursor.close()
+    conn.close()
+
+    # Quick validation of correct credentials
+    if (username == dbUser and password == dbPassword) :
+        return "Yes"
+
+    return "No"
