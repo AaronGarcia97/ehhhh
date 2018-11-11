eb4716668b690ae8822951ff34b6e9139c5d5055
danieltijerina <dtg.danieltijerina@gmail.com>
2018-11-10 19:13:40 -0600

    Added jsonifySession function to utility.py

--- utility.py
+++ utility.py
@@ -1,3 +1,5 @@
+import json
+
 def getTable(registrationType) :
     if ( registrationType == 'U' ) :
         return "Cliente"
@@ -7,3 +9,13 @@ def getTable(registrationType) :
         return "Admin"
     else :
         return "None"
+
+def jsonifySession(data):
+        info={}
+        info['id']=data[0]
+        info['username']=data[1]
+        info['password']=data[2]
+        info['name']=data[3]
+        info['lastName']=data[4]
+        info['sex']=data[5]
+        return json.dumps(info)    
