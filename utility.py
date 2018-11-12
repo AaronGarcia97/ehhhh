aee054b5b947e549948bdab6fefedf2859b8c821
danieltijerina <dtg.danieltijerina@gmail.com>
2018-11-12 17:54:57 -0600

    Added jsonifyTaxiSession

--- utility.py
+++ utility.py
@@ -10,7 +10,7 @@ def getTable(registrationType) :
     else :
         return "None"
 
-def jsonifySession(data):
+def jsonifyUserSession(data):
         info={}
         info['id']=data[0]
         info['username']=data[1]
@@ -18,4 +18,15 @@ def jsonifySession(data):
         info['name']=data[3]
         info['lastName']=data[4]
         info['sex']=data[5]
-        return json.dumps(info)    
+        return json.dumps(info) 
+
+def jsonifyTaxiSession(data):
+        info={}
+        info['id']=data[0]
+        info['username']=data[1]
+        info['password']=data[2]
+        info['name']=data[3]
+        info['lastName']=data[4]
+        info['sex']=data[5]
+	info['id_admin']=data[6]
+        return json.dumps(info)
